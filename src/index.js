e8e89e1d9ed334d51d587ec200aa808581415c86
Richard <richard.brown0807@gmail.com>
2022-08-11 21:21:25 -0500

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+})
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault() 
+    taskAdder(e.target.description.value)
+})
+
+function taskAdder(toDo) {
+  const p = document.createElement("p");
+  const object = document.createElement("button");
+  p.textContent = toDo
+  object.textContent = "x"
+
+  object.addEventListener("click", deleteParent)
+  p.appendChild(object)
+  tasks.appendChild(p)
+}
+
+function deleteParent(e) {
+e.target.parentNode.remove()
+}
+ 
+
+
+
+
